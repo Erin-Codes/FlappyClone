301ce14017c044999c4d61fe10f6407878d55611
Liano <130678376+LianoHyperion@users.noreply.github.com>
2024-05-30 16:58:48 +0200

    Intial commit for flappy app.

---
+++ flappy_bird/app.js
@@ -0,0 +1,19 @@
+function preload () {
+    /* A preload() function to bring in images for our application, such as the background*/
+
+}
+
+
+function create () {
+    /* A create() function to generate elements that will appear in our game,
+    such as images that were brought in from the preload() function.*/
+
+}
+
+
+function update () {
+    /* An update() function will be used to update the "bird"object in the game.
+    For example, if we want it to fly up when we press the "up" arrow button, we will do this inside the 'update()' function.
+    It runs continually, and will respond to any user interactions or variables that change.*/
+
+}
